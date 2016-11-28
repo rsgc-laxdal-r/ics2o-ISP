2a03648251e1ad97e5e97ce19e2e977c2d5952a1
Student <student@macintosh.local>
2016-11-28 11:37:32 -0500

    Added Storyboard Layout

--- FortressItemGenerator/ViewController.swift
+++ FortressItemGenerator/ViewController.swift
@@ -10,8 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var roundedCornerButton: UIButton!
+    @IBOutlet weak var roundedCornerButton2: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        roundedCornerButton.layer.cornerRadius = 6
+        roundedCornerButton2.layer.cornerRadius = 6
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
